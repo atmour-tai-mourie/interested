1675388c3f947d14243680909a6f028eca4dd25c
atmour-tai-mourie <taii.mourie@gmail.com>
2022-04-26 18:13:52 +1200

    add loan form outline (incomplete)

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import Header from './Header'
+import LoanForm from './LoanForm'
 
 function App() {
   return (
     <>
       <div className="app">
         <Header />
+        <LoanForm />
       </div>
     </>
   )
---
+++ client/components/LoanForm.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function LoanForm() {
+  return (
+    <div>
+      <h2>New Loan</h2>
+      <form>
+        <label></label>
+        <input />
+        <label></label>
+        <input />
+        <label></label>
+        <input />
+        <label></label>
+      </form>
+    </div>
+  )
+}
+
+export default LoanForm
