ef0cad139819d2f1bac0bcd03a0fb751c071ded6
atmour-tai-mourie <taii.mourie@gmail.com>
2022-05-03 22:10:56 +1200

    learning how to use grommet for forms

--- client/components/LoanForm.jsx
+++ client/components/LoanForm.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
+import { FormField, TextInput, Select } from 'grommet'
 
 function LoanForm() {
   return (
     <div>
       <h2>New Loan</h2>
       <form>
-        <label></label>
-        <input />
-        <label></label>
-        <input />
-        <label></label>
-        <input />
-        <label></label>
+        <FormField label="Term">
+          <Select options={[1, 2, 3, 4, 5]} defaultValue={1} />
+        </FormField>
+        <FormField label="Interest Rate">
+          <TextInput />
+        </FormField>
+        <FormField label="Loan Amount">
+          <TextInput />
+        </FormField>
+        <FormField label="Payment Frequency">
+          <Select
+            options={['Weekly', 'Fortnightly', 'Monthly']}
+            defaultValue="Weekly"
+          />
+        </FormField>
       </form>
     </div>
   )
