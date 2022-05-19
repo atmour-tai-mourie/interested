5190e8be4cb6e3525ec431169965304194309b56
atmour-tai-mourie <taii.mourie@gmail.com>
2022-05-19 21:07:44 +1200

    fix form formating and use values

--- client/components/LoanForm.jsx
+++ client/components/LoanForm.jsx
@@ -1,31 +1,53 @@
 import React from 'react'
 import { FormField, TextInput, Select } from 'grommet'
 
-function LoanForm({loanDetails, setLoanDetails}) {
-  const {interestRate, term, loanAmount, paymentFrequency} = loanDetails
-
-
-
-
-
-
+function LoanForm({ handleFormChange, loanDetails }) {
+  console.log(loanDetails)
   return (
     <div className="form__div">
       <h2>New Loan</h2>
       <form>
-        <FormField label="Term">
-          <Select options={[1, 2, 3, 4, 5]} defaultValue={1} />
+        <FormField label="Loan Duration">
+          <Select
+            onChange={handleFormChange}
+            name="duration"
+            options={[1, 2, 3, 4, 5]}
+            defaultValue={loanDetails.duration}
+          />
         </FormField>
         <FormField label="Interest Rate">
-          <TextInput name='interestRate' value={interestRate}/>
+          <TextInput
+            type="number"
+            icon="% p.a."
+            reverse
+            onChange={handleFormChange}
+            name="interestRate"
+            value={loanDetails.interestRate}
+          />
         </FormField>
         <FormField label="Loan Amount">
-          <TextInput name='loanAmount' value={loanAmount}/>
+          <TextInput
+            type="number"
+            icon="$"
+            onChange={handleFormChange}
+            name="loanAmount"
+            value={loanDetails.loanAmount}
+          />
+        </FormField>
+        <FormField label="Remaining Loan Term">
+          <TextInput
+            type="number"
+            onChange={handleFormChange}
+            name="term"
+            value={loanDetails.term}
+          />
         </FormField>
         <FormField label="Payment Frequency">
           <Select
+            onChange={handleFormChange}
+            name="paymentFrequency"
             options={['Weekly', 'Fortnightly', 'Monthly']}
-            defaultValue="Weekly"
+            defaultValue={loanDetails.paymentFrequency}
           />
         </FormField>
         <button className="form__button">View Loan Details</button>
