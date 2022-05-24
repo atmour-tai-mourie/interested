a1611fbd81f1bd4085c8e3ca4117cd345a76e7ea
atmour-tai-mourie <taii.mourie@gmail.com>
2022-05-24 22:31:28 +1200

    add new forecast components

---
+++ client/components/ForecastForm.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function ForecastForm() {
+  return <></>
+}
+
+export default ForecastForm
---
+++ client/components/ForecastResults.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function ForecastResults() {
+  return <></>
+}
+
+export default ForecastResults
